15581384dd40cf70ddc166d6a331acedb24c4f4f
Alfie Freeman <runticle@gmail.com>
2018-12-10 18:16:36 +0000

    updates snapshots and mocks results props in quiz.test.js
    Co-Authored-By: Felix Dalby Bowler <felixjtdb@gmail.com>

--- src/actions/quiz.js
+++ src/actions/quiz.js
@@ -2,6 +2,8 @@ import store from '../store/configureStore'
 
 export const loadQuestion = () => ({
     type: 'LOAD_QUESTION',
+
+    // THESE WILL COME FROM SOME ASSETS PLACE LADS
     payload: {
       question: 'What is a ethereum',
       answers: ['a fruit', 'a bird', 'a weapon', 'a revolution'],
--- src/components/__tests__/quiz.test.js
+++ src/components/__tests__/quiz.test.js
@@ -6,25 +6,25 @@ import Results from '../quiz/Results'
 
 describe('Quiz', () => {
   it('should have a title box', () => {
-    const wrapper = shallow(<Quiz />)
+    const wrapper = shallow(<Quiz results={[1,2,3]}u/>)
 
     expect(wrapper.text()).toContain('End of level Quiz')
   })
 
   it('matches last snapshot', () => {
-    const wrapper = shallow(<Quiz />)
+    const wrapper = shallow(<Quiz results={[1,2,3]}/>)
 
     expect(wrapper).toMatchSnapshot()
   })
 
   it('should render a QuestionBox component', () => {
-    const wrapper = shallow(<Quiz />)
+    const wrapper = shallow(<Quiz results={[1,2,3]} />)
 
     expect(wrapper.containsMatchingElement(<QuestionBox/>)).toEqual(true);
   })
 
   it('should render a Results component', () => {
-    const wrapper = shallow(<Quiz />)
+    const wrapper = shallow(<Quiz results={[1,2,3]} />)
 
     expect(wrapper.containsMatchingElement(<Results />)).toEqual(true);
   })
