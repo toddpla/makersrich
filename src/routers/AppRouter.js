2e1dafd70355484062c98c0e21d931b20dbc16af
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-10 15:08:10 +0000

    added gamepage route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory'
 import LoginPage from '../components/LoginPage'
+import GamePage from '../components/GamePage'
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute'
@@ -13,6 +14,7 @@ const AppRouter = () => (
     <div className="container-fluid">
       <Switch>
         <PublicRoute path="/" component={LoginPage} exact={true}/>
+        <PublicRoute path="/game" component={GamePage} exact={true}/>
         <Route component={NotFoundPage} />
       </Switch>
     </div>
