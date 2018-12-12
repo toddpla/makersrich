1dc5efd550b1f5da60e6f458f5af21bd01eaf6e6
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-12 18:44:26 +0000

    added minable to mapReducer
    Co-Authored-By: Alfie <runticle@gmail.com>

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -1,14 +1,22 @@
 import collectables from '../data/maps/level1/collectables'
+import minable from '../data/maps/level1/minable'
 
 const mapReducerDefaultState = {
-  collectables
+  collectables,
+  minable
 }
 
 export default (state=mapReducerDefaultState, action) => {
   switch (action.type) {
     case 'COLLECT_ITEM':
       return {
+        ...state,
         collectables: action.collectables
+      }
+    case 'DIG_TILE':
+      return {
+        ...state,
+        minable: action.minable,
       }
     default:
       return state
