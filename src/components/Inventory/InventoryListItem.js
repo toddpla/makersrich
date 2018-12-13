adf3a2365378fe0112f02a21becb82565c058451
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-13 18:15:40 +0000

    created player inventory
    Co-Authored-By: Alfie <runticle@gmail.com>

--- src/components/Inventory/InventoryListItem.js
+++ src/components/Inventory/InventoryListItem.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import './InventoryListItem.css'
 
 class InventoryListItem extends Component {
+
+  changeMessage = () => {
+    if (this.props.item.properties && this.props.item.properties[0].value) {
+      this.props.handleMessage(this.props.item.properties[0].value)
+    } else {
+      this.props.handleMessage(`This is a ${this.props.item.type}`)
+    }
+  }
 
   render() {
     console.log(this.props.item);
     return (
-      <div className="inventory-list-item">
-        <h3>{this.props.item.type}</h3>
-        <p>{this.props.item.properties && this.props.item.properties[0].value}</p>
+      <div className="inventory-list-item" onClick={this.changeMessage}>
+        <div className={this.props.item.type}></div>
       </div>
     );
   }
