53b6c4e24c0b486d3570f68b3d7323b2f9dbc938
Alfie Freeman <runticle@gmail.com>
2018-12-13 15:57:01 +0000

    adds styling for the quiz!

--- src/components/quiz/Question.js
+++ src/components/quiz/Question.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Question extends React.Component {
   render() {
     return (
-      <div>
+      <div className="quiz-question-box">
         {this.props.question}
       </div>
     )
