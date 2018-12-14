58e0eac29623869c4ec22c00694ba65ae820ac8d
Alfie Freeman <runticle@gmail.com>
2018-12-14 12:42:21 +0000

    adds shop item images and new shop object array

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { startAddInventoryItem } from '../../actions/auth'
+import './shop.css'
 
 import ShopList from './ShopList'
 import ShopMessage from './ShopMessage'
