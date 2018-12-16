4aa37022ba1cb6c17253c0f57257eede38f400f9
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-16 15:48:38 +0000

    adds player with inventory data

---
+++ src/test/fixtures/playerWithInventory.js
@@ -0,0 +1,29 @@
+export default {
+  cash: 0,
+  displayName: 'Frankie',
+  left: 0,
+  level: 1,
+  top: 0,
+  state: 'online',
+  uid: '1',
+  inventory : {
+    bean: [{
+      type: 'bean',
+      properties: [{
+        value: 'This is a bean'
+      }]
+    }],
+    key: [{
+      type: 'key',
+      properties: [{
+        value: 'This is a key'
+      }]
+    }],
+    ruby: [{
+      type: 'ruby',
+      properties: [{
+        value: 'This is a ruby'
+      }]
+    }],
+  }
+}
