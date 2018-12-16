30002b1f32a97ec7afb89cad3d78078ac085b04d
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-16 16:28:42 +0000

    added test for item with no message

--- src/components/Inventory/__tests__/InventoryListItem.test.js
+++ src/components/Inventory/__tests__/InventoryListItem.test.js
@@ -17,7 +17,20 @@ test('it renders InventoryListItem', () => {
   expect(wrapper).toMatchSnapshot()
 })
 
-test('#changeMessage changes message in inventory', () => {
+test('#changeMessage changes message in inventory with item messsage', () => {
+  item = player.inventory.bean[0]
+  wrapper = shallow(
+    <InventoryListItem item={item} handleMessage={handleMessage}/>
+  )
   wrapper.find('.inventory-list-item').simulate('click')
   expect(handleMessage).toHaveBeenLastCalledWith("This is a bean")
+})
+
+test('#changeMessage shows type of item when item has no message', () => {
+  item = player.inventory.key[0]
+  wrapper = shallow(
+    <InventoryListItem item={item} handleMessage={handleMessage}/>
+  )
+  wrapper.find('.inventory-list-item').simulate('click')
+  expect(handleMessage).toHaveBeenLastCalledWith("This is a key")
 })
