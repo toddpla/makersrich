f381008fd77547206ca90e7f28026b3935bba2b0
Todd Langford-Archer <toddpla@gmail.com>
2018-12-16 22:57:22 +0000

    Deployed Battle Firebase Functions
    This includes two functions to be deployed to the Firebase console. The 
    functions are used for starting a battle a posting the choice of weapon. 
    The logic for how the game is instatiated will is controlled mainly by 
    the data strucutre and the event listeners.

--- functions/index.js
+++ functions/index.js
@@ -3,32 +3,45 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+exports.startBattle = functions.https.onCall((data, context) => {
+  const playerOneUid = data.playerOneUid
+  const playerTwoUid = data.playerTwoUid
+  admin.database().ref(`/battles/${playerOneUid}`).set({'opponentUid': playerTwoUid})
+  admin.database().ref(`/battles/${playerTwoUid}`).set({'opponentUid': playerOneUid})
+})
 
-// exports.checkPlayerCollision = functions.database.ref('/players')
-//   .onWrite('child_changed', (snapshot, context) => {
-//     let player = snapshot.val()
-//     player.uid = snapshot.ref
-//     functions.database.ref('/players/')
-//       .where('top', '==', player.top)
-//       .where('left', '==', player.left)
-//       .where('level', '==', player.level)
-//       .limitToLast(1)
-//       .once('value').then((snapshot) => {
-//         let opponent;
-//         snapshot.forEach((childSnapshot) => {
-//           opponent = childSnapshot.val()
-//           opponent.uid = childSnapshot.key
-//         })
-//       if (opponent !== undefined) {
-//         functions.database.ref('/games/rps').push({
-//           [player.uid]: {
-//             hand: "empty"
-//           },
-//           [opponent.uid]: {
-//             hand: "empty"
-//           }
-//         })
-//       }
-//       return
-//     }).catch((e) => console.log(e))
-//   })
+exports.useWeapon = functions.https.onCall((data, context) => {
+  const weapon = data.weapon;
+  const uid = context.auth.uid;
+  return admin.database().ref(`/battles/${uid}`).once('value').then(snap => {
+    const battle = snap.val()
+    if (battle.opponentUid !== undefined) {
+      return admin.database().ref(`/battles/${battle.opponentUid}/weapon`).once('value').then(weaponSnap => {
+        const weaponsMatrix = {'rock': ['scissors'], 'paper': ['rock'], 'scissors': ['paper']}
+        const opponentWeapon = weaponSnap.val();
+        if (Object.keys(weaponsMatrix).includes(opponentWeapon)) {
+          if (weaponsMatrix[weapon].includes(opponentWeapon)) {
+            const winningUid = uid
+            const losingUid = battle.opponentUid
+          } else {
+            const winningUid = battle.opponentUid
+            const losingUid = uid
+          }
+          admin.database().ref(`/players/${winningUid}/cash`).once('value').then(cashSnap => {
+            return admin.database().ref(`/players/${winningUid}/cash`).set(cashSnap.val() + 10)
+          }).catch(err => console.log(err))
+          admin.database().ref(`/players/${losingUid}/cash`).once('value').then(opponentCashSnap => {
+            return admin.database().ref(`/players/${losingUid}/cash`).set(opponentCashSnap.val() - 10)
+          }).catch(err => console.log(err))
+          admin.database().ref(`/battles/${winningUid}`).remove()
+          admin.database().ref(`/battles/${losingUid}`).remove()
+          return admin.database().ref('/notifications/').push({message: `${winningUid} just beat ${losingUid} in a battle`})
+        } else {
+          return 'weapon not recognised'
+        }
+      })
+    } else {
+      return admin.database().ref(`/battles/${uid}/weapon`).set(weapon)
+    }
+  })
+})
