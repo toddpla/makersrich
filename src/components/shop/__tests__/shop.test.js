2e68a930d9f30c359825488c6666ed805e8e5c17
Alfie Freeman <runticle@gmail.com>
2018-12-16 17:01:30 +0000

    adds test for handleSelect to Shop.test.js

--- src/components/shop/__tests__/shop.test.js
+++ src/components/shop/__tests__/shop.test.js
@@ -5,13 +5,22 @@ import {Shop} from '../Shop'
 import ShopList from '../ShopList'
 import ShopMessage from '../ShopMessage'
 
-test('it renders with an ShopList and ShopMessage', () => {
+import { shallow } from 'enzyme'
+
   const player = {
     inventory: {
       ruby: [],
       bean: [],
       key: [],
       miscellaneous: []
+    }
+  }
+
+  const item = {
+    type: 'miscellaneous',
+    properties: {
+      price: 10,
+      name: 'item'
     }
   }
 
@@ -25,10 +34,27 @@ test('it renders with an ShopList and ShopMessage', () => {
     ]
   }
 
+test('it renders with an ShopList and ShopMessage', () => {
+
   const renderer = new ShallowRenderer()
   renderer.render(<Shop player={player} shop={shop}/>)
   const result = renderer.getRenderOutput()
   expect(result.props.children[0].props.children).toEqual("Welcome to Muxworthy's General Store!")
   expect(result.props.children[1].type).toEqual(ShopList)
   expect(result.props.children[2].type).toEqual(ShopMessage)
+})
+
+test('#handleSelect changes selected state', () => {
+
+  let wrapper = shallow(
+    <Shop
+      player={player}
+      shop={shop}
+    />
+  )
+
+  const instance = wrapper.instance()
+  expect(wrapper.state.selected).toEqual(undefined)
+  instance.handleSelect(item)
+  expect(instance.state.selected).toEqual(item)
 })
