1060a80f4125b4faa656c86ed0d0f9de82cf473c
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-16 19:54:18 +0000

    test for form submit

--- src/components/quiz/__tests__/AddQuestionPage.test.js
+++ src/components/quiz/__tests__/AddQuestionPage.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme'
-import AddQuestionPage from '../AddQuestionPage'
+import { AddQuestionPage } from '../AddQuestionPage'
+import event from '../../../test/fixtures/addQuestionEvent'
 
-let wrapper
+let wrapper, startAddQuestion
 
 beforeEach(function() {
+  startAddQuestion = jest.fn()
   wrapper = shallow(
-    <AddQuestionPage />
+    <AddQuestionPage startAddQuestion={startAddQuestion} />
   )
 });
 
@@ -14,4 +16,20 @@ describe('AddQuestionPage', function() {
   it('renders', function() {
     expect(wrapper).toMatchSnapshot()
   });
+
+  it('calls #startAddQuestion', function() {
+    const question = {
+      question:'What is 1 + 1?',
+      answers: [
+        '1',
+        '2',
+        '3',
+        '4'
+      ],
+      correctAnswer: '2'
+    }
+    wrapper.find('form').simulate('submit', event)
+    expect(startAddQuestion).toHaveBeenLastCalledWith(question)
+  });
+
 });
