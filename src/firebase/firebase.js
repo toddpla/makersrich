6d4003920238e3bf73d55d6d40ac5dc257f1d712
Todd Langford-Archer <toddpla@gmail.com>
2018-12-16 23:00:33 +0000

    Removed comments from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,5 @@
 import * as firebase from 'firebase';
 
-// development environemnt
-// const config = {
-//   apiKey: "AIzaSyCujvkUHO5pOnh5hQ44JvJILFEoR916zvs",
-//   authDomain: "proof-of-work.firebaseapp.com",
-//   databaseURL: "https://proof-of-work.firebaseio.com",
-//   projectId: "proof-of-work",
-//   storageBucket: "",
-//   messagingSenderId: "852831365773"
-// };
-
-// environemnt variables
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
