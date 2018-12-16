2f272ac29b3a2aea368a4773ec70c60eabea43aa
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-12-16 18:21:38 +0000

    test - #handleClick calls #startSendResult
    test has been re-structured to use describe blocks

--- src/components/quiz/__tests__/Quiz.test.js
+++ src/components/quiz/__tests__/Quiz.test.js
@@ -2,34 +2,60 @@ import React from 'react';
 import { shallow } from 'enzyme'
 import { Quiz } from '../Quiz'
 
-let wrapper, startSendResult, startGetQuestion, quiz
+describe('Quiz', () => {
+  let wrapper, startSendResult, startGetQuestion, quiz, auth
 
-beforeEach(function() {
-  startGetQuestion = jest.fn()
+  beforeEach(function() {
+    startGetQuestion = jest.fn()
+    startSendResult = jest.fn()
+    auth = {
+      uid:'123'
+    }
+    quiz = {
+      id: 8,
+      answers: [
+        'one',
+        'two',
+        'three',
+        'four'
+      ],
+      correctAnswer: 'two',
+      question: 'What comes after one?'
+    }
+    wrapper = shallow(
+      <Quiz
+        quiz={quiz}
+        auth={auth}
+        startGetQuestion={startGetQuestion}
+        startSendResult={startSendResult}
+      />
+    )
+  });
 
-  quiz = {
-    id: 8,
-    answers: [
-      'one',
-      'two',
-      'three',
-      'four'
-    ],
-    correctAnswer: 'two',
-    question: 'What comes after one?'
-  }
-  wrapper = shallow(
-    <Quiz
-      quiz={quiz}
-      startGetQuestion={startGetQuestion}
-    />
-  )
-});
+  test('it renders Quiz', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
 
-test('it renders Quiz', () => {
-  expect(wrapper).toMatchSnapshot()
-})
+  test('it calls #startGetQuestion', () => {
+    expect(startGetQuestion).toHaveBeenCalled()
+  })
 
-test('it calls #startGetQuestion', () => {
-  expect(startGetQuestion).toHaveBeenCalled()
+  describe('#handleClick', () => {
+    let instance, submission
+
+    beforeEach(function() {
+      submission = {
+        uid: '123',
+        questionId: 8,
+        result: true
+      }
+
+      instance = wrapper.instance()
+      instance.handleClick()
+    });
+
+    it('calls #startSendResult', () => {
+      expect(startSendResult).toHaveBeenLastCalledWith(submission)
+    });
+  });
 })
