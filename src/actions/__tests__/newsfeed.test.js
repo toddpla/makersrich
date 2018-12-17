e487716a804070e16535f9f2141bd963ad56cb6c
Todd Langford-Archer <toddpla@gmail.com>
2018-12-17 18:04:58 +0000

    Newsfeed

--- src/actions/__tests__/newsfeed.test.js
+++ src/actions/__tests__/newsfeed.test.js
@@ -1,8 +1,26 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import databsae from '../../firebase/firebase'
+import database from '../../firebase/firebase'
 import * as actions from '../newsfeed'
 
 const createMockStore = configureMockStore([thunk])
 
-let players
+let currentPlayer, uid, defaultAuthState, store;
+
+beforeEach(done => {
+  uid = 'sometestuid'
+  defaultAuthState = { auth: { uid } };
+  store = createMockStore(defaultAuthState)
+})
+
+test('should add message to the newsfeed', (done) => {
+  const message = "test message"
+  actions.startSendNewsfeedMessage(message)
+  database.ref('newsfeed').once('value').then(snap => {
+    const messages = []
+    snap.forEach(childSnap => {
+      messages.push(childSnap.val())
+    })
+    expect(messages[0].message).toEqual(message)
+  })
+})
