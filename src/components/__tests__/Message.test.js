6f5e0a200dbe276d88fbfd534b1e9ac336ec5a7c
felixjtdb <felixjtdb@gmail.com>
2018-12-18 18:26:47 +0000

    correcting failing tests

--- src/components/__tests__/Message.test.js
+++ src/components/__tests__/Message.test.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import Message from '../Message'
+import InformationMessage from '../InformationMessage'
 import renderer from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
 
 
 test('renders correctly', () => {
-  const wrapper = renderer.create(<Message message={'message'} />)
+  const wrapper = renderer.create(<InformationMessage message={'message'} />)
   expect(wrapper).toMatchSnapshot()
 });
 
 test('renders with a message prop', () => {
 
   const renderer = new ShallowRenderer()
-  renderer.render(<Message message={'message'} />)
+  renderer.render(<InformationMessage message={'message'} />)
   const result = renderer.getRenderOutput()
   console.log(result.props);
   expect(result.props.children).toEqual("message")
