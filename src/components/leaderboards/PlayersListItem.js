39c6bf4faf992c3f4156305da43b142674ae987e
Alfie Freeman <runticle@gmail.com>
2018-12-19 08:47:29 +0000

    adds some sorting to the columns

--- src/components/leaderboards/PlayersListItem.js
+++ src/components/leaderboards/PlayersListItem.js
@@ -4,13 +4,12 @@ export class PlayersListItem extends Component {
 
   render() {
     return (
-      <tr class="leaderboard-table-row">
-
-        <td class="leaderboard-table-cell" ><img style={{height: "10px"}}src={`images/${this.props.state}.png`} alt="status"/></td>
-        <td class="leaderboard-table-cell table-displayName" >{this.props.displayName}</td>
-        <td class="leaderboard-table-cell" >{this.props.cash}</td>
-        <td class="leaderboard-table-cell" >{this.props.inventory && this.props.inventory.ruby && Object.keys(this.props.inventory.ruby).length}</td>
-        <td class="leaderboard-table-cell" >{this.props.inventory && this.props.inventory.bean && Object.keys(this.props.inventory.bean).length}</td>
+      <tr className="leaderboard-table-row">
+        <td className="leaderboard-table-cell" ><img style={{height: "10px"}}src={`images/${this.props.state}.png`} alt="status"/></td>
+        <td className="leaderboard-table-cell table-displayName" >{this.props.displayName}</td>
+        <td className="leaderboard-table-cell" >{this.props.cash}</td>
+        <td className="leaderboard-table-cell" >{this.props.inventory && this.props.inventory.ruby && Object.keys(this.props.inventory.ruby).length}</td>
+        <td className="leaderboard-table-cell" >{this.props.inventory && this.props.inventory.bean && Object.keys(this.props.inventory.bean).length}</td>
       </tr>
     );
   }
