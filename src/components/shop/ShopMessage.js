a821e6dcf35c59ea505d5f412ffab1dbe3854641
Alfie Freeman <runticle@gmail.com>
2018-12-19 09:49:17 +0000

    CANNOT BUY THINGS FROM MUXOWRTHYS IF YOU DON'T HAVE THE MONEY....

--- src/components/shop/ShopMessage.js
+++ src/components/shop/ShopMessage.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 class ShopMessage extends Component {
 
+  hasEnoughMoney = (price) => {
+    return this.props.cash > price
+  }
+
   render() {
     if(this.props.item !== undefined){
       return (
@@ -10,8 +14,12 @@ class ShopMessage extends Component {
           <div id="item-image" className={`shop-selected-item ${this.props.item.properties[0].type}`}></div>
           <div id="muxworthys-opinion">{this.props.item.properties[0].value}</div>
           <div id="item-price">Price: {this.props.item.properties[0].price}Cash</div>
+          {
+            this.hasEnoughMoney(this.props.item.properties[0].price) ?
+            <div id="purchase-item-button" onClick={this.props.handlePurchase}>BUY</div> :
+            <div id="no-money-message">YOU GOT NO MONEY, BETTER GO DIGGIN'</div>
+          }
 
-          <div id="purchase-item-button" onClick={this.props.handlePurchase}>BUY</div>
         </div>
       )
     }
