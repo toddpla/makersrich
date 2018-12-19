fb1cb851422f003b878226f1ef2c7156821f0807
rbbri <robin.s.johnson@gmail.com>
2018-12-19 17:46:58 +0000

    Fixes typos

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -8,14 +8,14 @@ class Instructions extends Component {
         <h2>Instructions</h2>
         <p>
         Welcome to MakersRich!<br/>
-        Aim of the game - Find rubies and beans and complete quizes to level<br/>
-        BEWARE! Other players are also competing with you and won't be afraid to challenge you to a showdown of rock, paper scissors an take your stuff!
+        Aim of the game - Find rubies and beans and complete quizzes to level up<br/>
+        BEWARE! Other players are also competing with you and won't be afraid to challenge you to a showdown of rock, paper, scissors and take your stuff!
         </p>
         <h3>Controls</h3>
         <p>Use the arrow keys to move around</p>
         <p>Press 'Q' to interact with signs</p>
         <p>Press 'E' to dig</p>
-        <p>Press 'I' to open</p>
+        <p>Press 'I' to open inventory</p>
         <p>Press 'Spacebar' to close any pop-ups</p>
       </div>
     );
