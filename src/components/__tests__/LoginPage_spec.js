37357ec388cd6dc55b4fd9f1fc03e2f32f254437
Alfie Freeman <runticle@gmail.com>
2018-12-19 09:18:15 +0000

    Merge branch 'testingcontrolpanel' of github.com:toddpla/proof-of-work into testingcontrolpanel

--- src/components/__tests__/LoginPage_spec.js
+++ src/components/__tests__/LoginPage_spec.js
@@ -2,9 +2,19 @@ import React from 'react'
 import { LoginPage } from '../LoginPage'
 import renderer from 'react-test-renderer';
 import { Header } from '../Header'
+import { shallow } from 'enzyme'
 
+let handleGithubLogin = jest.fn()
+let handleGoogleLogin = jest.fn()
 
 test('renders correctly', () => {
   const loginPage = renderer.create(<LoginPage />).toJSON();
   expect(loginPage).toMatchSnapshot();
+});
+
+test('should call startLogin on button click', () => {
+  const wrapper = shallow(<LoginPage />);
+  console.log(wrapper.find('button'));
+  wrapper.find('#google-login').simulate('click');
+  expect(handleGoogleLogin).toHaveBeenCalled();
 });
