999c8dc53d9fdf62d896e20ff80df8d5804a7fff
Alfie Freeman <runticle@gmail.com>
2018-12-20 12:22:04 +0000

    quiz at 93% coverage

--- src/components/quiz/__tests__/Quiz.test.js
+++ src/components/quiz/__tests__/Quiz.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme'
-import { Quiz } from '../Quiz'
+import { Quiz, mapDispatchToProps } from '../Quiz'
+
 
 describe('Quiz', () => {
   let wrapper, startSendResult, startGetQuestion, quiz, auth, startDebitPlayer,
-      clearQuiz, canAffordQuestion, sendResultToFirebase
+      clearQuiz, canAffordQuestion, sendResultToFirebase, updatePlayer, startUpdatePlayer, closeModal
 
   beforeEach(function() {
     clearQuiz = jest.fn()
+    closeModal = jest.fn()
+    updatePlayer = jest.fn()
+    startUpdatePlayer = jest.fn()
     startGetQuestion = jest.fn()
     canAffordQuestion = jest.fn()
     startSendResult = jest.fn()
@@ -41,6 +45,7 @@ describe('Quiz', () => {
         startSendResult={startSendResult}
         startDebitPlayer={startDebitPlayer}
         clearQuiz={clearQuiz}
+        closeModal={closeModal}
       />
     )
   });
@@ -51,7 +56,7 @@ describe('Quiz', () => {
 
   it('calls #canAffordQuestion', () => {
     let instance = wrapper.instance()
-    expect(instance.canAffordQuestion).toEqual(expect.any(Function))
+    expect(instance.canAffordQuestion()).toEqual(true)
   })
 
   test('it calls #startGetQuestion', () => {
@@ -72,6 +77,21 @@ describe('Quiz', () => {
       instance.handleClick(1)
     });
 
+    it('calls #removeQuiz', () => {
+      document.body.innerHTML =
+      '<div id="quiz-container">' +
+      '  <div id="quiz" />' +
+      '  <div id="button" />' +
+      '</div>'
+      instance.removeQuiz()
+      expect(document.getElementById('quiz')).toEqual(null)
+    })
+
+    it('#sendResultToFirebase calls startSendResult', () => {
+      instance.sendResultToFirebase(submission)
+      expect(startSendResult).toHaveBeenCalled()
+    })
+
     it('calls #startSendResult', () => {
       expect(startSendResult).toHaveBeenCalled()
     });
@@ -80,8 +100,34 @@ describe('Quiz', () => {
       expect(startGetQuestion).toHaveBeenLastCalledWith(submission.uid)
     });
 
+    it('hanldeClose calls clearquiz and closeModal', () => {
+      instance.handleClose()
+      expect(clearQuiz).toHaveBeenCalled()
+      expect(closeModal).toHaveBeenCalled()
+    })
+
     it('calls #startDebitPlayer', () => {
       expect(startDebitPlayer).toHaveBeenLastCalledWith(10)
-    });
-  });
+    })
+  })
+})
+
+test('mapDispatchToProps', () => {
+
+  const dispatch = jest.fn()
+
+  expect(mapDispatchToProps(dispatch).startSendResult('result'))
+  expect(mapDispatchToProps(dispatch).startGetQuestion())
+  expect(mapDispatchToProps(dispatch).startUpdatePlayer('updates'))
+  expect(mapDispatchToProps(dispatch).startDebitPlayer(10))
+  expect(mapDispatchToProps(dispatch).clearQuiz())
+  expect(mapDispatchToProps(dispatch).updatePlayer('updates'))
+
+  expect(dispatch.mock.calls[0][0]).toEqual(expect.any(Function))
+  expect(dispatch.mock.calls[1][0]).toEqual(expect.any(Function))
+  expect(dispatch.mock.calls[2][0]).toEqual(expect.any(Function))
+  expect(dispatch.mock.calls[3][0]).toEqual(expect.any(Function))
+  expect(dispatch.mock.calls[4][0]).toEqual({"quiz": {}, "type": "CLEAR_QUIZ"})
+  expect(dispatch.mock.calls[5][0]).toEqual({"type": "UPDATE_PLAYER", "updates": "updates"})
+
 })
